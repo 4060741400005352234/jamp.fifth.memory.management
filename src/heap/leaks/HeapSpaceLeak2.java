0403dc3b38ba08e18087671c250f22a936b215d7
foxygun <foxygun@bk.ru>
2016-01-19 10:48:56 +0600

    Fifth -leaks

--- src/heap/leaks/HeapSpaceLeak2.java
+++ src/heap/leaks/HeapSpaceLeak2.java
@@ -1,13 +1,17 @@
 package heap.leaks;
 
+import java.lang.ref.SoftReference;
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 
 public class HeapSpaceLeak2 {
 
-    public static void main(String[] args) {
-        List<Object> list = new ArrayList<Object>();
+    private static List<Object> list = new ArrayList<Object>();
+
+    public static void main(String[] args) throws InterruptedException {
         while(true) {
+            Thread.sleep(1);
             list.add(new Object());
         }
     }
--- src/heap/leaks/StackOverflowExample.java
+++ src/heap/leaks/StackOverflowExample.java
@@ -9,7 +9,8 @@ public class StackOverflowExample {
     private static class A {
         private B bInstance = null;
 
-        public A() {
+        public A() throws InterruptedException {
+            Thread.sleep(500);
             bInstance = new B();
         }
     }
@@ -17,7 +18,8 @@ public class StackOverflowExample {
     private static class B {
         private A aInstance = null;
 
-        public B() {
+        public B() throws InterruptedException {
+            Thread.sleep(500);
             aInstance = new A();
         }
     }
